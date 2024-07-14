d9fec66dc5d64c7f48bd98bc8102f7dee9e19078
lucky319 <lakshmisunkara092@gmail.com>
2024-07-14 09:52:47 +0530

    python pre

--- Lakshmi_Test.py
+++ Lakshmi_Test.py
@@ -1,8 +1,17 @@
-print('Created for Git Practice.')
-print(" added new line for git practice")
-
-
-print("Adding below for loop for python practice.")
-for i in range(10):
-    print(i)
+# __add__ method 
+class Test:
     
+    def __init__(self, a ,b):
+        self.a = a
+        self.b = b
+        
+    def __add__(self):
+        return f"Adding a and b values : {Test(self.a, self.b)}"
+    def __str__(self):
+        return f"m str Method Test({self.a}, {self.b})"
+    
+t1 = Test(20,30)
+print(t1)
+print(t1.__add__())
+t2= Test(4,5)
+print(t2)
